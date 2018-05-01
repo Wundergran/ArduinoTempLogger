eacf6dfaf4cc31c4eb95efd611fd94ce77e48473
frode <frode.standal94@gmail.com>
2018-05-01 11:06:34 +0200

    add logic for verifying data

--- Node/app.js
+++ Node/app.js
@@ -1,8 +1,35 @@
 const SerialPort = require('serialport');
-var sPort = new SerialPort('COM6', {
-  baudRate: 9600
-});
 
+var sPort = new SerialPort('COM6', {
+  baudRate: 57600
+});
 sPort.on('data', function(data) {
-    console.log(data.toString('utf8'));
+  dataRead(data)
 })
+
+var tempLog = []
+
+function dataRead (data) {
+  if (verifyData(data)) {
+    var json = {
+      time: Date.now(),
+      temp: data.toString()
+    }
+    console.log(json)
+  } else {
+    console.log('Threw away garbage: ' + data.toString())
+  }
+}
+
+function verifyData (data) {
+  var value = data.toString()
+  
+  return value.length === 5
+}
+
+function saveData (jsonData) {
+  tempLog.push(jsonData)
+  if (tempLog.length > 60) { // save to disk
+
+  }
+}
