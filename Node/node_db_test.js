e0c34a06c3e959dc3b632fbe0440d6a215f9c8fa
frodstan <frode.standal@p4.no>
2018-05-02 10:49:05 +0200

    cleanup test db script

--- Node/node_db_test.js
+++ Node/node_db_test.js
@@ -1,18 +1,12 @@
 const sqlite = require('sqlite')
 const dbPromise = sqlite.open('./testdb.sqlite', { Promise })
 const dbTable = 'tempdata'
-
-const testData = {
-	time: 12444453653123,
-	temp: 23.45
-}
 tryDb()
 
 async function tryDb () {
-	console.log('async')
 	try {
 		const db = await dbPromise
-		const temps = await db.get('SELECT * FROM ' + dbTable)
+		const temps = await db.all('SELECT * FROM ' + dbTable)
 		console.log(temps)
 	} catch (err) {
 		console.log(err)
