3258678ac3e4b15e2b98d7edf8d5e5ccc75c3ff2
frodstan <frode.standal@p4.no>
2018-05-02 10:44:25 +0200

    add request to testdb

--- Node/node_db_test.js
+++ Node/node_db_test.js
@@ -1,7 +1,20 @@
-import sqlite from 'sqlite';
-const dbPromise = sqlite.open('./database.sqlite', { Promise })
+const sqlite = require('sqlite')
+const dbPromise = sqlite.open('./testdb.sqlite', { Promise })
+const dbTable = 'tempdata'
 
 const testData = {
 	time: 12444453653123,
 	temp: 23.45
+}
+tryDb()
+
+async function tryDb () {
+	console.log('async')
+	try {
+		const db = await dbPromise
+		const temps = await db.get('SELECT * FROM ' + dbTable)
+		console.log(temps)
+	} catch (err) {
+		console.log(err)
+	}
 }
