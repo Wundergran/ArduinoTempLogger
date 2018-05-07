6905f2e58ce451ec3b97f04d41654d66f308e733
frode <frode.standal94@gmail.com>
2018-05-07 22:46:50 +0200

    emit hello on connect

--- Node/node_test.js
+++ Node/node_test.js
@@ -2,12 +2,14 @@ var server = require('http').createServer();
 var io = require('socket.io')(server);
 
 io.on('connection', function(client){
-	console.log('connecting....')
-	//client.on('event', function(data){});
-	//client.on('disconnect', function(){});
+	console.log('connected to localhost')
+	client.emit('hello', { hello: 'world' })
+
+	client.on('event', function(data){});
+	client.on('disconnect', function(){});
 });
 try{
-	io.origins('*:*')
+	io.origins('localhost:8080')
 	io.listen(3000)
 } catch (err) {
 	console.log(err)
