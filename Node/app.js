6fa3fc2bf1f3cfc532de76ac2dcfe75344597a51
frode <frode.standal94@gmail.com>
2018-05-08 22:02:35 +0200

    add event emitter/listener

--- Node/app.js
+++ Node/app.js
@@ -1,13 +1,17 @@
 const sqlite = require('sqlite')
 const SerialPort = require('serialport')
+const Events = require('events')
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 
 const dbPromise = sqlite.open('./database.sqlite', { Promise })
+const eventEmitter = new Events.EventEmitter()
 
 const TABLE = 'tempdata'
 const FLD_TIME = 'time'
 const FLD_TEMP = 'temp'
+
+const DATA_EVENT = 'new-data'
 
 var wsClient = null
 var latestTemp = null // Default void
@@ -20,8 +24,13 @@ var sPort = new SerialPort('COM5', {
 
 io.on('connection', function(client){
 	console.log('connected to localhost')
-  wsClient = client
-  client.emit('last-temp', latestTemp)
+  // wsClient = client
+  // client.emit('last-temp', latestTemp)
+  client.on('listen', function (args) {
+    eventEmitter.on(DATA_EVENT, function () { // listen for database changes
+      
+    })
+  })
 	client.on('disconnect', function(){ 
     wsClient = null
     console.log('client disconnected') 
@@ -66,9 +75,11 @@ function verifyData (data) {
 async function saveData (jsonData) {
   latestTemp = jsonData
   try {
+    const query = `INSERT INTO ${TABLE} (${FLD_TIME}, ${FLD_TEMP}) VALUES (${jsonData.time}, ${jsonData.temp})`
 		const db = await dbPromise
-    await db.exec(`INSERT INTO ${TABLE} (${FLD_TIME}, ${FLD_TEMP}) VALUES (${jsonData.time}, ${jsonData.temp})`)
-    console.log(`INSERT: ${jsonData.time} - ${jsonData.temp}`)
+    await db.exec(query)
+    eventEmitter.emit(DATA_EVENT)
+    console.log('EXECUTED: ' + query)
 	} catch (err) {
 		console.log(err)
 	}
