db0551a9915f19f2fe7017ed2222c6701e720844
frode <frode.standal94@gmail.com>
2018-05-10 15:01:55 +0200

    remove chart logic from vue component

--- vue-cli/src/components/Chart.vue
+++ vue-cli/src/components/Chart.vue
@@ -1,21 +1,12 @@
+<template>
+  
+</template>
 <script>
-import Vue from 'vue'
-import VueChartJs from 'vue-chartjs'
-Vue.component('chart', {
-  name: 'Chart',
-	props:['data'],
-  extends: VueChartJs.Line,
-  mounted () {
-    this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'Data One',
-          backgroundColor: '#f87979',
-          data: [40, 39, 10, 40, 39, 80, 40]
-        }
-      ]
-    }, {responsive: true, maintainAspectRatio: false})
-  }
-})
+export default {
+  
+}
 </script>
+<style scoped>
+
+</style>
+
