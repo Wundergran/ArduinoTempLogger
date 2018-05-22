02e1319123f97d48d3ddf3a2c7c37bad5535d05b
frodstan <frode.standal@p4.no>
2018-05-22 15:36:11 +0200

    change chart color

--- vue-cli/src/components/Chart.vue
+++ vue-cli/src/components/Chart.vue
@@ -4,7 +4,8 @@
       :chart-data="computedData" 
       :options="options"
       :height="700"
-      :width="1200"></line-chart>
+      :width="1200">
+    </line-chart>
   </div>
 </template>
 <script>
@@ -34,7 +35,7 @@ export default {
             labels: this.data.map(a => this.getDateStrFor(a.time)),
             datasets: [{
               label: 'Temperature',
-              backgroundColor: '#90a4ae',
+              backgroundColor: '#3F51B5',
               data: this.data.map(a => a.temp)
             }]
           }
