e519dc92bf54fede5450bf15d4ad022274179861
frodstan <frode.standal@p4.no>
2018-05-29 15:25:51 +0200

    open server to local network

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -7,7 +7,7 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
-Vue.use(VueSocketio, 'http://localhost:3000');
+Vue.use(VueSocketio, 'http://192.168.101.15:3000');
 Vue.use(Vuetify, {
   theme: { primary: "#3F51B5", secondary: "#283593", accent: "#9c27b0", error: "#f44336", warning: "#ffeb3b", info: "#2196f3", success: "#4caf50" }
 })
